c4dccbdad0bdfd959d88aa46e06feadc9fda6a31
albertozaranza <albertozaranza@outlook.com>
2018-03-12 15:23:10 -0300

    Criação das Migrations

---
+++ WebApplication/WebApplication/Migrations/20180312182216_CreateDatabase.cs
@@ -0,0 +1,153 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace WebApplication.Migrations
+{
+    public partial class CreateDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pessoas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pessoas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Clientes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(nullable: true),
+                    PessoaId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clientes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Clientes_Pessoas_PessoaId",
+                        column: x => x.PessoaId,
+                        principalTable: "Pessoas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Funcionarios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(nullable: true),
+                    PessoaId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Funcionarios", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Funcionarios_Pessoas_PessoaId",
+                        column: x => x.PessoaId,
+                        principalTable: "Pessoas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Carros",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    ClienteId = table.Column<int>(nullable: false),
+                    Placa = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Carros", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Carros_Clientes_ClienteId",
+                        column: x => x.ClienteId,
+                        principalTable: "Clientes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Registros",
+                columns: table => new
+                {
+                    CarroId = table.Column<int>(nullable: false),
+                    FuncionarioId = table.Column<int>(nullable: false),
+                    DataEntrada = table.Column<string>(nullable: true),
+                    DataSaida = table.Column<string>(nullable: true),
+                    Id = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Registros", x => new { x.CarroId, x.FuncionarioId });
+                    table.ForeignKey(
+                        name: "FK_Registros_Carros_CarroId",
+                        column: x => x.CarroId,
+                        principalTable: "Carros",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Registros_Funcionarios_FuncionarioId",
+                        column: x => x.FuncionarioId,
+                        principalTable: "Funcionarios",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Carros_ClienteId",
+                table: "Carros",
+                column: "ClienteId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Clientes_PessoaId",
+                table: "Clientes",
+                column: "PessoaId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Funcionarios_PessoaId",
+                table: "Funcionarios",
+                column: "PessoaId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Registros_FuncionarioId",
+                table: "Registros",
+                column: "FuncionarioId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Registros");
+
+            migrationBuilder.DropTable(
+                name: "Carros");
+
+            migrationBuilder.DropTable(
+                name: "Funcionarios");
+
+            migrationBuilder.DropTable(
+                name: "Clientes");
+
+            migrationBuilder.DropTable(
+                name: "Pessoas");
+        }
+    }
+}
---
+++ WebApplication/WebApplication/Models/StoreContext.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.Models
+{
+    public class StoreContext : DbContext
+    {
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Carro> Carros { get; set; }
+        public DbSet<Funcionario> Funcionarios { get; set; }
+        public DbSet<Pessoa> Pessoas { get; set; }
+        public DbSet<Registro> Registros { get; set; }
+
+        private static IConfigurationRoot Configuration;
+
+        public StoreContext()
+        {
+            var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json");
+
+            Configuration = builder.Build();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connection = (Configuration.GetConnectionString("StoreDB"));
+            optionsBuilder.UseSqlServer(connection);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Registro>()
+                .HasKey(r => new { r.CarroId, r.FuncionarioId });
+
+            modelBuilder.Entity<Registro>()
+                .HasOne(f => f.Funcionario)
+                .WithMany(reg => reg.Registros)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
