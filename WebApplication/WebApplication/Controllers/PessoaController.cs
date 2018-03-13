f8dbd9e037942fd71213c880d84b8f8c7a977a88
albertozaranza <albertozaranza@outlook.com>
2018-03-12 21:47:00 -0300

    Adição de Controllers

---
+++ WebApplication/WebApplication/Controllers/PessoaController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers
+{
+    [Route("api/Pessoas")]
+    public class PessoaController : BaseController<Pessoa>
+    {
+    }
+}
